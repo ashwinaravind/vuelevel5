99c8bb8fca2b7b2427ac90259d518478a52756e4
ashwinaravind <ashwinaravind@gmail.com>
2018-12-26 03:24:13 -0800

    working version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <p>configured on github</p>
   <div id="app">
+    <p>configured on github</p>
     <nav>
       <router-link to ="/">Home</router-link>
       <router-link to ="/about">About</router-link>
